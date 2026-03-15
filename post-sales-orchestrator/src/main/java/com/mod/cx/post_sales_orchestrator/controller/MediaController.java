a2ff1be285bfd13a819b676ff04c76ebad63052c
Wahab-Fincity <abdulwahab.shaik@modlix.com>
2026-03-15 12:54:15 +0530

    added base service and base controller to reduce common boiler plate code

--- post-sales-orchestrator/src/main/java/com/mod/cx/post_sales_orchestrator/controller/MediaController.java
+++ post-sales-orchestrator/src/main/java/com/mod/cx/post_sales_orchestrator/controller/MediaController.java
@@ -1,12 +1,14 @@
 package com.mod.cx.post_sales_orchestrator.controller;
 
 import com.mod.cx.post_sales_orchestrator.service.ImageKitService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Map;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/media")
 public class MediaController {
@@ -17,7 +19,7 @@ public class MediaController {
         this.imageKitService = imageKitService;
     }
 
-    @PostMapping("/upload/userId")
+    @PostMapping("/upload/{userId}")
     public ResponseEntity<Map<String, String>> uploadDocument(
             @PathVariable String userId,
             @RequestParam("file") MultipartFile file,
@@ -26,10 +28,12 @@ public class MediaController {
         try{
             return ResponseEntity.ok(imageKitService.uploadPrivateAsset(file, userId, category));
         } catch (Exception e) {
+            log.error("Failed to upload document for userId: {}", userId, e);
             return ResponseEntity.internalServerError().build();
         }
     }
 
+    @GetMapping("/url")
     public ResponseEntity<String> getUrl(@RequestParam("path") String filePath) {
         return ResponseEntity.ok(imageKitService.generateSecureUrl(filePath));
     }
